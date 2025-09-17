a8a357724854a813116c6637f2f05db8cffb9ef7
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-17 13:36:12 -0500

    [main] init state

---
+++ source/funkin/InitState.hx
@@ -0,0 +1,15 @@
+package funkin;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+class InitState extends FlxState
+{
+
+        override function create() {
+                super.create();
+
+                FlxG.switchState(() -> new funkin.menus.TitleState());
+        }
+        
+}
